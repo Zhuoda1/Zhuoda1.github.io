c13242179f60531f0d3f8b09d5eed1f2a524e567
Zhuoda <lzd020815@gmail.com>
2023-05-04 15:13:53 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3bf608b320563b093d39a0231d178753.js"
+  "/precache-manifest.4e208efc0a4e858afd98c5da64c23367.js"
 );
 
 self.addEventListener('message', (event) => {
